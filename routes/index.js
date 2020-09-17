51f6193095a268334b670ed501156b3536446fd1
cesarcarrizo <cesarcarrizo95@gmail.com>
2020-09-17 13:46:00 -0600

    Creacion del repositorio, servidor usando express

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+module.exports = () => {
+
+    // create routing modules for each specific URI
+
+    return router;
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+/* MAIN SERVER */
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mssql = require("mssql");
+
+const router = require("./routes");
+
+const PORT = process.env.PORT || 3000;
+const localhost = "127.0.0.1";
+
+const app = express();
+
+// check this for security stuff
+app.set("trust proxy", 1);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use("/static", express.static(path.join(__dirname, "./resources")));
+
+app.use("/", router());
+
+app.listen(PORT, () => console.log(`Servidor escuchando por peticiones en ${localhost}:${PORT}`));
