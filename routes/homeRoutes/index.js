6fda5caf6737e5c9e6f5c11edd70cfb810b22639
cesarcarrizo <cesarcarrizo95@gmail.com>
2020-10-27 19:22:57 -0600

    trabajando frontend

--- routes/homeRoutes/index.js
+++ routes/homeRoutes/index.js
@@ -13,8 +13,21 @@ module.exports = (db) => {
                 res.send('<h1>Acceso no autorizado, por favor iniciar sesión!</h1>');
             } else {
                 let guestData = await db.guest(global.authId);
-                console.log(guestData); 
-                res.render('pages/homePage');
+                //console.log(guestData);
+
+                // enviamos la vista como respuesta junto con la data de la base de datos
+                res.render('pages/homePage', {data: {
+                    cedula_usu_pk: guestData.cedula_usu_pk,
+                    nombre_comp_usu: guestData.nombre_comp_usu,
+                    user_usu: guestData.user_usu,
+                    passwd_usu: guestData.passwd_usu,
+                    email_usu: guestData.email_usu,
+                    pregunta_usu: guestData.pregunta_usu,
+                    respuesta_usu: guestData.respuesta_usu,
+                    ep_safe_code_usu: guestData.ep_safe_code_usu,
+                    ep_pass_usu: guestData.ep_pass_usu,
+                    rol_usu: guestData.rol_usu,
+                  }});
             }
         });
 
